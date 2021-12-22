35695da1720521103dc39e24d29dccf205b5358c
LiorAki <lior21231@gmail.com>
2021-12-22 18:42:37 +0200

    added option to save to local storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,40 +1,38 @@
-
 export const locService = {
-    getLocs,
-    addLoc,
-    saveToStorage,
-    loadFromStorage
-}
+  getLocs,
+  addLoc,
+  saveToStorage,
+  loadFromStorage,
+};
 
-var gIdx;
+var gIdx = 1;
 
 const locs = [
-    { id: 1, name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { id: 2, name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+  { id: gIdx++, name: "Greatplace", lat: 32.047104, lng: 34.832384 },
+  { id: gIdx++, name: "Neveragain", lat: 32.047201, lng: 34.832581 },
+];
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs);
+    }, 2000);
+  });
 }
 
-
-function addLoc(locationName, pos){
-    let {lat, lng} = pos;
-    locs.push({name: locationName, lat, lng});
-    return Promise.resolve(locs);
+function addLoc(locationName, pos) {
+  let { lat, lng } = pos;
+  locs.push({ id: gIdx++, name: locationName, lat, lng });
+  
+  return Promise.resolve(locs);
 }
-
 
 function saveToStorage(key, val) {
-    localStorage.setItem(key, JSON.stringify(val))
+  localStorage.setItem(key, JSON.stringify(val));
 }
 
 function loadFromStorage(key) {
-    var val = localStorage.getItem(key)
-    console.log ('the userDB is',JSON.parse(val)) 
-    return JSON.parse(val)
+  var val = localStorage.getItem(key);
+  console.log("the userDB is", JSON.parse(val));
+  return JSON.parse(val);
 }
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,91 +1,74 @@
 import { locService } from "./loc.service.js";
 
-
 export const mapService = {
-    initMap,
-    addMarker,
-    panTo,
-    onMapClick
-}
+  initMap,
+  addMarker,
+  panTo,
+  onMapClick,
+};
 
 var gMap;
 
-
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap');
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available');
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
-            console.log('Map!', gMap);
-        })
-        
+  console.log("InitMap");
+  return _connectGoogleApi().then(() => {
+    console.log("google available");
+    gMap = new google.maps.Map(document.querySelector("#map"), {
+      center: { lat, lng },
+      zoom: 15,
+    });
+    console.log("Map!", gMap);
+  });
 }
 
 function addMarker(loc, title) {
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
-        title,
-        // icon: {
-        //     path: 'custom icon path',
-        //     fillColor: '#000000',
-        //     labelOrigin: new google.maps.Point(26.5, 20),
-        //     anchor: new google.maps.Point(26.5, 43),
-        //     scale: 1,
-        //   }
-        
-    });
-    return marker;
+  var marker = new google.maps.Marker({
+    position: loc,
+    map: gMap,
+    title,
+    // icon: {
+    //     path: 'custom icon path',
+    //     fillColor: '#000000',
+    //     labelOrigin: new google.maps.Point(26.5, 20),
+    //     anchor: new google.maps.Point(26.5, 43),
+    //     scale: 1,
+    //   }
+  });
+  return marker;
 }
-
 
 function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng);
-    gMap.panTo(laLatLng);
-    addMarker(laLatLng);
-
+  var laLatLng = new google.maps.LatLng(lat, lng);
+  gMap.panTo(laLatLng);
+  addMarker(laLatLng);
 }
-
-
 
 function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyCuK9aoRAmaj-w7I8tKFEB3tM66yzfXOfM'; //TODO: Enter your API Key
-    var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-    elGoogleApi.async = true;
-    document.body.append(elGoogleApi);
+  if (window.google) return Promise.resolve();
+  const API_KEY = "AIzaSyCuK9aoRAmaj-w7I8tKFEB3tM66yzfXOfM"; //TODO: Enter your API Key
+  var elGoogleApi = document.createElement("script");
+  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+  elGoogleApi.async = true;
+  document.body.append(elGoogleApi);
 
-    return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve;
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
+  return new Promise((resolve, reject) => {
+    elGoogleApi.onload = resolve;
+    elGoogleApi.onerror = () => reject("Google script failed to load");
+  });
 }
 
-function onMapClick(){
-    gMap.addListener('click', function(ev) {
-        // let locationName = prompt('Name of selected location:');
-        const pos = ev.latLng.toJSON();
-        if(pos) {
-            panTo(pos.lat, pos.lng);
-        }
-    });
-}
 
-// Saves to local storage
-// function onMapClick(onSuccess){
-//     gMap.addListener('click', function(ev) {
-//         const pos = ev.latLng.toJSON();
-//         let locName = prompt('Name this location');
-//         if (locName) {
-//             locService.addLoc(locName, pos)
-//             panTo(pos.lat, pos.lng, locName);
-//             onSuccess();
-//         }
-//     });
-// }
+function onMapClick(onSuccess) {
+  gMap.addListener("click", ev => {
+    const pos = ev.latLng.toJSON();
+    let canSave = confirm("Would you like to save this location?");
+    if (canSave) {
+      let locName = prompt("Name this location");
+      if (locName) {
+        locService.addLoc(locName, pos);
+        panTo(pos.lat, pos.lng, locName);
+        onSuccess();
+      }
+    }
+  });
+}
